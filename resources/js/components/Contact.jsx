23213c8a8cf60d91941a2db9f638193fd1de235c
tevinuwijesinghe <tevinuwijesignhe@gmail.com>
2026-09-13 21:54:23 +0530

    CV updated

--- resources/js/components/Contact.jsx
+++ resources/js/components/Contact.jsx
@@ -344,7 +344,7 @@ export default function Contact() {
 
                     <div className="cv-btn-wrapper">
     <a
-        href="/Tevinu_Manuditha_Wijesinghe_Resume_2026-09-06.pdf"
+        href="/Tevinu_Manuditha_Wijesinghe_2026-09-06.pdf"
         download
         className="cv-btn"
     >
